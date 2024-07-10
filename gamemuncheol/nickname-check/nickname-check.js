82cb4c860fc9919cac940cae21541705915a3806
rookedsysc <rookedsysc36@gmail.com>
2024-07-10 17:02:21 +0900

    feat: Nickname 확인 API 부하테스트 작성

--- gamemuncheol/nickname-check/nickname-check.js
+++ gamemuncheol/nickname-check/nickname-check.js
@@ -1,15 +1,14 @@
-import http from 'k6/http';
-import { check } from 'k6';
+import http from "k6/http";
+import { check, sleep } from "k6";
 
 export let options = {
-  vus: 1,      // 동시에 실행할 가상 사용자 수
-  iterations: 1,  // 반복 횟수 (총 요청 수 : 반복 횟수 / 동시 사용자 수)
+  vus: 10,      // 동시에 실행할 가상 사용자 수
+  iterations: 10,  // 반복 횟수 (총 요청 수 : 반복 횟수 / 동시 사용자 수)
   duration: '10m', // 최대 테스트 지속 시간
 };
 
 export default function() {
-  const url = 'https://api.gamemuncheol.com:8080/open-api/posts/page/new';
-  // const url = 'https://api.gamemuncheol.com:8080/open-api/posts/page/grill?cursor=47.00009944569329&size=10';
+  const url = 'https://api.gamemuncheol.com:8080/open-api/members/nickname/m1kk2j1I29';
 
   const params = {
     headers: {
@@ -24,6 +23,3 @@ export default function() {
     'is status 200': (r) => r.status === 200,
   });
 }
-
-// 135809
-// 151
